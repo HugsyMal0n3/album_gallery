5426fbe717e16f4972285093153adb3a84ef16d1
HugsyMal0n3 <electricnicks@gmail.com>
2026-02-15 22:15:06 +0000

    feat(admin): register Album model with admin
    Customise admin to make album model display better
    for UI/UX

--- backend/albums/admin.py
+++ backend/albums/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Album
+
+
+@admin.register(Album)
+class AlbumAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "created_at"]
+    search_fields = ["title"]
+    ordering = ["-created_at"]
+    date_hierarchy = "created_at"
