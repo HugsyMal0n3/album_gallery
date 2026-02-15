73da542bf70145fcafc47449496b8a7b2727452a
HugsyMal0n3 <electricnicks@gmail.com>
2026-02-15 23:09:03 +0000

    feat(album): register AlbumViewSet urls

---
+++ backend/albums/urls.py
@@ -0,0 +1,7 @@
+from rest_framework.routers import DefaultRouter
+from .views import AlbumViewSet
+
+router = DefaultRouter()
+router.register(r"albums", AlbumViewSet, basename="album")
+
+urlpatterns = router.urls
