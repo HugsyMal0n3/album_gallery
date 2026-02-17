f7c873dcdd61c45db37138dacaa3ff4c390d8c5a
HugsyMal0n3 <electricnicks@gmail.com>
2026-02-17 19:21:16 +0000

    test(album): add database constraint tests
    Add unit tests to validate Album model database
    behaviour and integrity constraints.

--- backend/albums/tests.py
+++ backend/albums/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from django.db import IntegrityError, transaction
 
-# Create your tests here.
+from albums.models import Album
+
+
+class AlbumModelTests(TestCase):
+    """
+    Tests for Album model database behavioud and constraints
+    """
+
+    def test_album_with_valid_title_saves_successfully(self):
+        album = Album.objects.create(title="My Album")
+
+        self.assertEqual(album.title, "My Album")
+        self.assertIsNotNone(album.created_at)
+        self.assertIsNotNone(album.updated_at)
+
+    def test_album_title_cannot_be_empty_due_to_check_constraint(self):
+        with self.assertRaises(IntegrityError):
+            with transaction.atomic():
+                Album.objects.create(title="")
